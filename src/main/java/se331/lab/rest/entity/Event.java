1933b29502bdd24b903cac9a17e9bad0925d05d0
NNilawan <nilawan_l@cmu.ac.th>
2021-09-09 21:30:30 +0700

    Q1 1.1

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
+import se331.lab.rest.entity.Organizer;
+import se331.lab.rest.entity.Participant;
 
 @Data
 @Builder
@@ -24,5 +27,6 @@ public class Event {
     String date;
     String time;
     Boolean petAllowed;
-    String organizer;
+    Organizer organizer;
+    List<Participant> participants;
 }
---
+++ src/main/java/se331/lab/rest/entity/Participant.java
@@ -0,0 +1,11 @@
+package se331.lab.rest.entity;
+
+import java.util.List;
+
+public class Participant {
+    Long id;
+    String name;
+    String telNo;
+    List<Event> eventHistory;
+
+}
